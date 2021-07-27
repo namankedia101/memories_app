eef2f894520989a75030539c72e94c03b4798793
Your Name <you@example.com>
2021-07-28 00:09:05 +0530

    First Commit

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,6 +1,6 @@
 import mongoose  from "mongoose";
 
-import PostMessage from "./models/postMessage.js";
+import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req, res) => {
   try {
