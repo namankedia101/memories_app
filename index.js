d06a1dacfa093ab92575538bf4909f42852f2e2f
Your Name <you@example.com>
2021-08-16 00:43:42 +0530

    Fourth Commit- Resolved issues

--- index.js
+++ index.js
@@ -42,17 +42,15 @@ app.get("/api",(req,res)=>res.send("Hello to memories API"));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
 
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true, "useFindAndModify":false })
   .then(() =>
     app.listen(PORT, () => console.log("Server is running on port:"+ PORT))
   )
   .catch((error) => console.log(error.message));
-
-mongoose.set("useFindAndModify", false);
