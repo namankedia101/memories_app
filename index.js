7538e1d264d7c18b51ebdad4de4c9903613204d0
Your Name <you@example.com>
2021-08-15 15:06:28 +0530

    Fourth Commit- Resolved issues

--- index.js
+++ index.js
@@ -14,6 +14,16 @@ dotenv.config();
 app.use(bodyParser.json({ extended: true, limit: "30mb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "30mb" }));
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 // Have Node serve the files for our built React app
 app.use(express.static("client/build"));
