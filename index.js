25fd1d5c68de7e09aece29476a5282e6c80324c2
Your Name <you@example.com>
2021-08-16 14:28:18 +0530

    Fourth Commit- Resolved issues

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
 import path from "path";
+import { fileURLToPath } from 'url';
 
 import postRoutes from "./routes/posts.js";
 import userRoutes from "./routes/user.js";
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 dotenv.config();
 
 app.use(express.json({ extended: true, limit: "30mb" }));
@@ -42,7 +46,7 @@ app.get("/api",(req,res)=>res.send("Hello to memories API"));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
